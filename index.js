10222cf6c3db03973850ad8cda6d59b9f64990cc
Usman <usmansardar247@gmail.com>
2020-07-02 11:49:39 +0500

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dotenv.config();
 const serverSocket = require('./serverSocket')
 
 const app = require("./router/router");
-const port = process.env.PORT;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}..`);
 });
