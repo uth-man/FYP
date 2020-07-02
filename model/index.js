3c7da85555debd0b305e6afa38040bb637ece3ec
Usman <usmansardar247@gmail.com>
2020-07-02 20:10:04 +0500

    v1.1.08

--- model/index.js
+++ model/index.js
@@ -45,7 +45,7 @@ function createTableBookingDetails(req, res, next) {
 
 function createPassengerTable(req, res, next) {
   let sql =
-    "CREATE TABLE IF NOT EXISTS passenger (id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(255), email varchar(255) NOT NULL UNIQUE,phone varchar(255),password varchar(255),socketId varchar(255), creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, modificationDate TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, isPool TINYINT(1) NOT NULL DEFAULT 0)"
+    "CREATE TABLE IF NOT EXISTS passenger (id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(255), email varchar(255) NOT NULL UNIQUE,phone varchar(255),password varchar(255),socketId varchar(255), creationDate TIMESTAMP, modificationDate TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, isPool TINYINT(1) NOT NULL DEFAULT 0)"
   db.query(sql, (error, result) => {
     if (error) {
       console.log(error);
@@ -61,7 +61,7 @@ function createDriverTable(req, res, next) {
 
   let sql =
     "CREATE TABLE IF NOT EXISTS driver (id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(255),gender varchar(255),email varchar(255),phone varchar(255),password varchar(255),cnic varchar(255),drivingLicense varchar(255),vehicleEngine varchar(255),vehicleModel varchar(255),licensePlate varchar(255),vehicleClass varchar(255),vehicleType varchar(255)," +
-    "creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, modificationDate TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,isVerified TINYINT(1) NOT NULL DEFAULT 0,picture varchar(255), userCat varchar(255), isOnline TINYINT(1), currentLocation varchar(255),socketId varchar(255))"
+    "creationDate TIMESTAMP, modificationDate TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,isVerified TINYINT(1) NOT NULL DEFAULT 0,picture varchar(255), userCat varchar(255), isOnline TINYINT(1), currentLocation varchar(255),socketId varchar(255))"
   db.query(sql, (error, result) => {
 
     if (error) {
