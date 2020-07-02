840d621f11895d2d35fcc6167922c716d29ff0b7
Usman <usmansardar247@gmail.com>
2020-07-02 16:20:46 +0500

    v1.1.01

--- model/index.js
+++ model/index.js
@@ -58,6 +58,7 @@ function createPassengerTable(req, res, next) {
   })
 }
 function createDriverTable(req, res, next) {
+
   let sql =
     "CREATE TABLE IF NOT EXISTS driver (id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name varchar(255),gender varchar(255),email varchar(255),phone varchar(255),password varchar(255),cnic varchar(255),drivingLicense varchar(255),vehicleEngine varchar(255),vehicleModel varchar(255),licensePlate varchar(255),vehicleClass varchar(255),vehicleType varchar(255),creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, modificationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,isVerified TINYINT(1) NOT NULL DEFAULT 0,picture varchar(255), userCat varchar(255), isOnline TINYINT(1), currentLocation varchar(255),socketId varchar(255))"
   db.query(sql, (error, result) => {
