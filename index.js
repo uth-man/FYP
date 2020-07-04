5fb15de4276c71841db63ac8c75e647ad8b49776
Usman <usmansardar247@gmail.com>
2020-07-04 20:54:14 +0500

    live communication

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const serverSocket = require('./serverSocket')
 
-const app = require("./router/router");
+const app = require("./router/router").app;
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}..`);
